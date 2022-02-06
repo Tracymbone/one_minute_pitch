8ba0fa0ed24fb0b880d4d6fc65f8fadcd24f1397
moringa <tressy.mbone@student.moringaschool.com>
2022-02-06 15:15:36 +0300

    initial commit

--- run.py
+++ run.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
-@app.route('/')
-def landingpage():
-     return render_template('landingpage.html')
+@app.route('/home')
+def home():
+     return render_template('home.html')
  
 
 
-@app.route('/index')
+@app.route('/')
 def index():
     return render_template('index.html')
 
