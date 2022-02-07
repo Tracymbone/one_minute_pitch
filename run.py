61ab818e6d3a38aa52e493280df024a5624d24d6
moringa <tressy.mbone@student.moringaschool.com>
2022-02-07 17:31:20 +0300

    initial changes

--- run.py
+++ run.py
@@ -8,9 +8,32 @@ def home():
  
 
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login.html')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/signup.html')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/count.html')
+def count():
+    return render_template('count.html')
+
+
+
+@app.route('/logout.html')
+def logout():
+    return render_template('logout.html')
+
 
 
 @app.route('/contact')
