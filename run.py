9eba3fc61ed4ed63695874d0c42196ba52d20931
moringa <tressy.mbone@student.moringaschool.com>
2022-02-08 23:14:02 +0300

    made changes in run.py

--- run.py
+++ run.py
@@ -3,8 +3,49 @@ from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms .validators import InputRequired,Length,ValidationError
+
 
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']="postgresql+psycopg2://moringa:Access@localhost/tracy"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
+app.secret_key="Access"
+__tablename__="users"
+db=SQLAlchemy(app)
+bcrypt=Bcrypt(app)
+
+
+class user(db.Model,UserMixin):
+ id=db.Column(db.Integer,primary_key=True)
+ firstname=db.Column(db.String(50),nullable=False)
+ middlename=db.Column(db.String(50),nullable=False)
+ username=db.Column(db.String(50),nullable=False,unique=True)
+ email=db.Column(db.String(50),nullable=False,unique=True)
+ password=db.Column(db.String(50),nullable=False)
+ 
+ 
+ class registerForm():
+     firstname=StringField(validators=[InputRequired(),Length(min=4,max=20)])
+     middlename=StringField(validators=[InputRequired(),Length(min=4,max=20)])
+     username=StringField(validators=[InputRequired(),Length(min=4,max=20)])
+     email=StringField(validators=[InputRequired(),Length(min=4,max=20)])
+     password=StringField(validators=[InputRequired(),Length(min=4,max=20)])
+     
+     
+     def validate_email(self, email):
+         existing_user_email=user.query.filter(email=email.data).first()
+         if existing_user_email:
+             raise ValidationError("The email is already in use!")
+         
+ class loginForm():
+     username=StringField(validators=[InputRequired(),Length(min=4,max=20)])
+     email=StringField(validators=[InputRequired(),Length(min=4,max=20)])
+     password=StringField(validators=[InputRequired(),Length(min=4,max=20)])
+     
+     
+         
+        
 
 @app.route('/home')
 def home():
